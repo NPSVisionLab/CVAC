d8df19cb3d399f4c2edb4e174d31c9eb6e71cd44
matz <matz@10.0.1.5>
2013-03-21 11:10:08 -0700

    UnitTest additions, mostly from Tom. LibArchive HINTS restored.

--- CMakeModules/FindLibArchive.cmake
+++ CMakeModules/FindLibArchive.cmake
@@ -1,17 +1,21 @@
 #Locate the libArchive project
 
 FIND_PATH(LIBARCHIVE_INCLUDE archive.h
-           PATHS "/opt/local/libarchive/include" 
-                 "c:/Program files (x86)/libarchive/include"
-           DOC "Include directory for libarchive"
-           )
+          HINTS
+              ../CVAC_extras/include
+          PATHS
+              /opt/local
+              "C:/Program Files (x86)/libarchive/include"
+          DOC "Include directory for libarchive"
+         )
            
 # search for the libarchive library, first in the CVAC_extras folder,
 # then in the location related to the include file, then in default locations
 FIND_LIBRARY(LIBARCHIVE_LIBRARY NAMES archive
              HINTS
-             ../CVAC_extras/lib
-             ${LIBARCHIVE_INCLUDE}/../lib
-             PATHS "${SEARCH_PATH}/libarchive/lib"
+                ../CVAC_extras/lib
+                ${LIBARCHIVE_INCLUDE}/../lib
+             PATHS 
+                "C:/Program Files (x86)/libarchive/lib"
              DOC "Library directory for libarchive"
-             )
+            )
