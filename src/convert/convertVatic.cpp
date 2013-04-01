fc2763a7320335030707e93282b77946d7b58e61
jcschmid <jcschmid@nps.edu>
2013-04-01 09:40:43 -0700

    Parsing of Vatic text file lines.
    A start to filling in all the proper ice structures:
    LabelableList is returned, which contains the Labelables of type LabeledVideoSegment parsed from the file.

--- src/convert/convertVatic.cpp
+++ src/convert/convertVatic.cpp
@@ -1,4 +1,5 @@
 #include <convert/convertVatic.h>
+#include <util/FileUtils.h>
 #include <fstream>
 #include <iostream>
 
@@ -7,14 +8,48 @@ using namespace std;
 
 LabelableList* cvac::convert(std::string filePath) {
 
+	LabelableList* returnList = new LabelableList();  // push_back video labels into this list
 	fstream inputFile;
+	string inputLine, labelStr;
+	int trackId, xMin, yMin, xMax, yMax, frameId, isLostInt, isOccludedInt, isGeneratedInt;
+	boolean isLost = false, isOccluded = false, isGenerated = false;
+
+	returnList->clear();
 	inputFile.open(filePath.c_str(), ios::in);
     if(!inputFile.is_open()) {
-		printf("Could not open data file for convert: %s\n", filePath.c_str());
+		localAndClientMsg(VLogger::WARN, NULL, "Could not open data file for convert: %s\n", filePath.c_str());
     }
+	localAndClientMsg(VLogger::DEBUG_1, NULL, "Convert file is open.  %s\n", filePath.c_str());
+
+	// parse 9 ints (last 3 are boolean), then string
+	while(std::getline(inputFile, inputLine)) {
+
+		stringstream ss(inputLine);
+		ss >> trackId >> xMin >> yMin >> xMax >> yMax >> frameId >> isLostInt >> isOccludedInt >> isGeneratedInt >> labelStr;
+
+		if(isLostInt) {
+			isLost = true;
+		}
+		if(isLostInt) {
+			isLost = true;
+		}
+		if(isOccludedInt) {
+			isOccluded = true;
+		}
+		if(isGeneratedInt) {
+			isGenerated = true;
+		}
+
+		// Create new LabeledVideoSegment, fill in data, push_back to LabelableList
+		LabeledVideoSegment* baseSeg = new LabeledVideoSegment();
+		baseSeg->lab.name = trackId;
+		baseSeg->lab.hasLabel = true;
+
+		Labelable* vidLabel = baseSeg;
+		returnList->push_back(vidLabel);
+	}
 
 
-	LabelableList* returnList = NULL;
 	inputFile.close();
 	return(returnList);
 }
