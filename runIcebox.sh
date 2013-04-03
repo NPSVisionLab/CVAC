7c51328328955082bfaf31c74e6f1c60ae8544cf
Thomas Batcha <tomb@eris.ern.nps.edu>
2013-04-03 10:42:19 -0700

    removing these files

--- runIcebox.sh
+++
@@ -1,6 +0,0 @@
-export PATH=$PATH:/opt/local/bin
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/Users/tomb/git/myCVAC/CVAC/lib"
-cd /Users/tomb/git/myCVAC/CVAC
-/opt/Ice-3.4/bin/icebox --Ice.Config=config.icebox &
-echo CVAC services launched
-exit
