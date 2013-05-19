fc95bd874a5115b3369a8ea56de72fbda198a76e
matzk <kolsch@nps.edu>
2013-05-19 10:54:43 -0700

    easy! first nice demo

---
+++ src/easy/demo.py
@@ -0,0 +1,58 @@
+#
+# A demo for the Easy Computer Vision library.
+#
+
+# before interpreting this file, make sure this is set:
+# export PYTHONPATH="/opt/Ice-3.4.2/python:./src/easy"
+import sys, traceback
+sys.path.append('''.''')
+import Ice
+import Ice
+import IcePy
+import cvac
+import easy
+
+#
+# Obtain a set of labeled data from a Corpus,
+# print dataset information about this corpus
+#
+cs = easy.getCorpusServer("CorpusServer:default -p 10011")
+# corpus = easy.openCorpus( cs, "corpus/CvacCorpusTest.properties" )
+# corpus = easy.openCorpus( cs, "corporate_logos" );
+corpus = easy.openCorpus( cs, "trainImg" );
+categories, lablist = easy.getDataSet( cs, corpus )
+print 'Obtained {0} labeled artifact{1} from corpus "{2}":'.format(
+    len(lablist), ("s","")[len(lablist)==1], corpus.name );
+easy.printCategoryInfo( categories )
+
+#
+# add all samples from corpus to a RunSet,
+# also obtain a mapping from class ID to label name
+#
+runset, classmap = easy.createRunSet( categories )
+
+#
+# Make sure all files in the RunSet are available on the remote site;
+# it is the client's responsibility to upload them if not.
+#
+fileserver = easy.getFileServer("FileServer:default -p 10013")
+easy.putAllFiles( fileserver, runset )
+
+#
+# Connect to a trainer service, train on the RunSet
+#
+trainer = easy.getTrainer( "bowTrain:default -p 10003" )
+detectorData = easy.train( trainer, runset )
+print "Training model stored in file: " + easy.getFSPath( detectorData.file )
+
+#
+# Connect to a detector service,
+# test on the training RunSet for validation purposes;
+# The detect call takes the detector, the trained model, the
+# runset, and a mapping from purpose to label name
+#
+detector = easy.getDetector( "bowTest:default -p 10004" )
+results = easy.detect( detector, detectorData, runset, classmap )
+easy.printResults( results )
+
+quit()
--- test/UnitTests/python/FileServerTest.py
+++ test/UnitTests/python/FileServerTest.py
@@ -53,9 +53,9 @@ class FileServerTest(unittest.TestCase):
         existingFile = cvac.FilePath( testDir, "TestUsFlag.jpg" )
         noFile = cvac.FilePath( testDir, "NonExistentFile.jpg" )
         if not self.fs.exists( existingFile ):
-            raise RuntimeError("This file should exist: "+self.getRelPath(existingFile) )
+            raise RuntimeError("This file should exist: "+self.getFSPath(existingFile) )
         if self.fs.exists( noFile ):
-            raise RuntimeError("This file should not exist: "+self.getRelPath( noFile ) )
+            raise RuntimeError("This file should not exist: "+self.getFSPath( noFile ) )
 
     #
     # Test if we can get a file, copy bytes to a tempfile, compare
@@ -77,7 +77,7 @@ class FileServerTest(unittest.TestCase):
         # read the bytes from TestKrFlag.jpg
         testDir = cvac.DirectoryPath( "testImg" );
         origFilePath = cvac.FilePath( testDir, "TestKrFlag.jpg" )
-        origFS = self.dataDir+"/"+self.getRelPath( origFilePath )
+        origFS = self.dataDir+"/"+self.getFSPath( origFilePath )
         if not os.path.exists( origFS ):
             raise RuntimeError("Cannot obtain path to original file, see comments: "+origFS)
         forig = open( origFS )
@@ -87,7 +87,7 @@ class FileServerTest(unittest.TestCase):
         # and compare the result via file system access
         putFilePath = cvac.FilePath( testDir, "TargetFilename.jpg" )
         self.fs.putFile( putFilePath, bytes );
-        putFS = self.dataDir+"/"+self.getRelPath( putFilePath )
+        putFS = self.dataDir+"/"+self.getFSPath( putFilePath )
         if not os.path.exists( putFS ):
             raise RuntimeError("Cannot obtain path to put file on file system, see comments: "+putFS)
         if not self.filesAreEqual( origFS, putFS ):
@@ -111,14 +111,14 @@ class FileServerTest(unittest.TestCase):
     def getFileAndCompare( self, filePath ):
         bytes = self.fs.getFile( filePath )
         if not bytes:
-            raise RuntimeError("could not obtain file from '"+self.getRelPath( filePath ))
+            raise RuntimeError("could not obtain file from '"+self.getFSPath( filePath ))
 
         # Write bytes to a temp file and compare the contents to the orig.
         # Since this is a test, it's probably run in the build directory. We
         # need to know the path to the original file for the compare, but we
         # don't have easy access to the CVAC.DataDir variable.  Let's try to
         # figure it out from the 'pwd'
-        orig = self.dataDir+"/"+self.getRelPath( filePath )
+        orig = self.dataDir+"/"+self.getFSPath( filePath )
         if not os.path.exists( orig ):
             raise RuntimeError("Cannot obtain path to original file, see comments: "+orig)
         
@@ -139,7 +139,7 @@ class FileServerTest(unittest.TestCase):
         # delta = ''.join(x[2:] for x in diff if x.startswith('- '))
         return filecmp.cmp( fname1, fname2 )
 
-    def getRelPath(self, cvacPath):
+    def getFSPath(self, cvacPath):
         path = cvacPath.directory.relativePath+"/"+cvacPath.filename
         return path
 
