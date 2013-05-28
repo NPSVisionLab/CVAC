1fad52f33f0478b5124e6047f6ba95db907c3f85
matzk <kolsch@nps.edu>
2013-05-28 15:52:32 -0700

    service ports: 101xx; also corrected demo.py problems

--- src/easy/demo.py
+++ src/easy/demo.py
@@ -2,10 +2,12 @@
 # A demo for the Easy Computer Vision library.
 #
 
-import sys
+import sys, os
 
 #Add the lib/python directory to get packages
-sys.path.append('''../../lib/python''')
+thisPath = os.path.dirname(os.path.abspath(__file__))
+srcPath = os.path.abspath(thisPath+"/../../lib/python")
+sys.path.append(srcPath)
 import paths
 import cvac
 
@@ -23,12 +25,13 @@ easy.printResults( results )
 # Second, a quick way to train a detector.  The resulting model
 # can be used in place of the detector above.
 #
-categories, lablist = easy.getDataSet( "corpus/CvacCorpusTest" )
+# TODO: currently breaks because Caltech101 doesn't get extracted as expected
+#categories, lablist = easy.getDataSet( "corpus/CvacCorpusTest" )
 # categories, lablist = easy.getDataSet( "corpus/Caltech101.properties", createMirror=False )
-easy.printCategoryInfo( categories )
-runset = easy.createRunSet( categories["car_side"] )
-trainer = easy.getTrainer( "bowTrain:default -p 10003" )
-carSideModel = easy.train( trainer, runset )
+#easy.printCategoryInfo( categories )
+#runset = easy.createRunSet( categories["car_side"] )
+#trainer = easy.getTrainer( "bowTrain:default -p 10003" )
+#carSideModel = easy.train( trainer, runset )
 
 #
 # Third, a slower walk-through of functionality that digs a bit deeper.  All
@@ -40,7 +43,7 @@ cs = easy.getCorpusServer("CorpusServer:default -p 10011")
 #corpus = easy.openCorpus( cs, "corpus/CvacCorpusTest.properties" )
 #corpus = easy.openCorpus( cs, "corporate_logos" );
 corpus = easy.openCorpus( cs, "trainImg" );
-categories, lablist = easy.getDataSet( cs, corpus )
+categories, lablist = easy.getDataSet( corpus, corpusServer=cs )
 print 'Obtained {0} labeled artifact{1} from corpus "{2}":'.format(
     len(lablist), ("s","")[len(lablist)==1], corpus.name );
 easy.printCategoryInfo( categories )
@@ -57,13 +60,15 @@ classmap = res['classmap']
 # Make sure all files in the RunSet are available on the remote site;
 # it is the client's responsibility to upload them if not.
 #
-fileserver = easy.getFileServer( "FileService:default -p 10110" )
+host = "-h localhost"
+#host = "-h vision.nps.edu"
+fileserver = easy.getFileServer( "FileService:default -p 10110 " + host )
 easy.putAllFiles( fileserver, runset )
 
 #
 # Connect to a trainer service, train on the RunSet
 #
-trainer = easy.getTrainer( "bowTrain:default -p 10003" )
+trainer = easy.getTrainer( "bowTrain:default -p 10103 " + host )
 trainedModel = easy.train( trainer, runset )
 print "Training model stored in file: " + easy.getFSPath( trainedModel.file )
 
@@ -73,8 +78,8 @@ print "Training model stored in file: " + easy.getFSPath( trainedModel.file )
 # The detect call takes the detector, the trained model, the
 # runset, and a mapping from purpose to label name
 #
-detector = easy.getDetector( "bowTest:default -p 10004" )
-results = easy.detect( detector, trainedModel, runset, classmap )
-easy.printResults( results )
+detector = easy.getDetector( "bowTest:default -p 10104 " + host )
+results = easy.detect( detector, trainedModel, runset )
+easy.printResults( results, foundMap=classmap )
 
 quit()
