29324409055a1d8e17ff0dfd448132fc58e0c428
matzk <kolsch@nps.edu>
2013-06-28 19:19:37 -0700

    expandFilename replaced with getFSPath

--- test/trainerClient/trainerClient.cpp
+++ test/trainerClient/trainerClient.cpp
@@ -324,7 +324,7 @@ int TrainerClientApp::run(int argc, char* argv[])
 				videoLabel->sub.path.directory.relativePath = std::string(argv[2]);				
 				videoLabel->interp = cvac::DISCRETE;	//If a enum. variable is not initialized, it causes error in debug mode.
 				
- 				std::string _filepath = expandFilename(videoLabel->sub.path.directory.relativePath, CVAC_DataDir);
+ 				std::string _filepath = getFSPath(videoLabel->sub.path, CVAC_DataDir);
  				parseFrame_SBD(_filepath,"AnnotationShortTrain.txt",_vFrameListInput);								
 
 				for(unsigned int k=0;k<_vFrameListInput.size();k++)
