8f91bc5d0dfbcdf1bb606226489b2f98f972392c
matzk <kolsch@nps.edu>
2013-08-13 15:30:08 -0700

    archive test now deletes trainer.properties

--- test/UnitTests/FileUtilsTests.cpp
+++ test/UnitTests/FileUtilsTests.cpp
@@ -288,6 +288,8 @@ SUITE(UnitTests_cvac)
     CHECK( deleteFile( subDir+"/logTrain_svm.xml.gz") );
     CHECK( deleteFile( subDir+"/logTrain_Table.txt") );
     CHECK( deleteFile( subDir+"/logTrain_Vocabulary.xml.gz") );
+    CHECK( deleteFile( subDir+"/trainer.properties") );
+    CHECK( directoryExists(subDir) ); // we expect this temp dir to NOT delete itself
     CHECK( deleteDirectory( subDir) );
   }
 
