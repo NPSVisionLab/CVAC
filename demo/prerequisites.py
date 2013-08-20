572287746107bbd03d0fdd91097b05443ed309ea
matzk <kolsch@nps.edu>
2013-08-20 11:12:38 -0700

    print environment first, try imports later

--- demo/prerequisites.py
+++ demo/prerequisites.py
@@ -5,6 +5,23 @@
 #
 # matz 6/18/2013
 
+import os, sys
+from datetime import datetime, date
+print("Writing environment report file 'python_env.txt'...")
+repfile = open('python_env.txt', 'w')
+repfile.write('Your environment as found by demo/prerequisites.py.\n')
+repfile.write('Generated on ' + str(date.today()) + ' at ' + str(datetime.now()) + '.\n\n')
+repfile.write('Python version: ' + str(sys.version_info[0]) + '.' \
+               + str(sys.version_info[1]) + '.' \
+               + str(sys.version_info[2]) + '\n')
+repfile.write('Current working directory: '+ str(os.getcwd()) + '\n' )
+repfile.write('Python executable location and version: ' + sys.executable + ', ' + sys.version + '.\n')
+repfile.write('Python final sys.path: ' + str(sys.path) + '\n')
+repfile.write('Environment variables:\n')
+for key in sorted( os.environ ):
+    val = os.environ.get( key )
+    repfile.write('  ' + key + ' = ' + val + '\n')
+
 print("Trying to import Ice... ")
 try:
     import Ice
@@ -44,16 +61,3 @@ except ImportError as ex:
     print("  That's either /your/CVAC_dir/lib/python or /your/CVAC_dir/src/easy.")
     print("  Detailed error message: {0}".format( ex ));
 
-import os, sys
-from datetime import datetime, date
-print("Writing environment report file 'python_env.txt'...")
-repfile = open('python_env.txt', 'w')
-repfile.write('Your environment as found by demo/prerequisites.py.\n')
-repfile.write('Generated on ' + str(date.today()) + ' at ' + str(datetime.now()) + '.\n')
-repfile.write('Current working directory: '+ str(os.getcwd()) + '\n' )
-repfile.write('Python executable location and version: ' + sys.executable + ', ' + sys.version + '.\n')
-repfile.write('Python final sys.path: ' + str(sys.path) + '\n')
-repfile.write('Environment variables:\n')
-for key in sorted( os.environ ):
-    val = os.environ.get( key )
-    repfile.write('  ' + key + ' = ' + val + '\n')
