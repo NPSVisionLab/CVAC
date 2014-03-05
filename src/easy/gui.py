bf6ad30f8d48bfa5ba50448da63a8a1e77f6c207
matzk <mathias.kolsch@gmail.com>
2014-03-04 18:17:00 -0800

    installs app, starts gui if invoked direction (not open yet)

--- src/easy/gui.py
+++ src/easy/gui.py
@@ -6,23 +6,46 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
-        self.determineConfiguration()
+        self.uiPrerequisites()
+        self.uiStartStopServices()
         self.createWidgets()
 
     def createWidgets(self):
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.quitButton.grid()
 
-    def determineConfiguration(self):
-        sys.path.append('demo')
+    def uiPrerequisites(self):
         import prerequisites
-        print prerequisites.success
-        #text = tk.Text()
-        #text.insert(tk.END, 'hello')
-        #text.pack(side='left')
+        text = tk.Text()
+        if prerequisites.success:
+            text.insert(tk.END, 'prerequisites test succeeded')
+        else:
+            text.insert(tk.END, 'prerequisites test failed')
+        text.grid()
         self.detailsButton = tk.Button(self, text='Show details', 
                                        command=self.openEnv)
         self.detailsButton.grid()
+
+    def uiStartStopServices(self):
+        lockFileExists = os.path.isfile('.services_started.lock')
+        text = tk.Text()
+        if lockFileExists:
+            text.insert(tk.END, 'services have been started (lock file exists)')
+        else:
+            text.insert(tk.END, 'services have not been started (no lock file)')
+        text.grid()
+        self.startButton = tk.Button(self, text='start services', 
+                                     command=self.startStopServices(True))
+        self.startButton.grid()
+        self.stopButton = tk.Button(self, text='stop services', 
+                                     command=self.startStopServices(False))
+        self.stopButton.grid()
+
+    def startStopServices(self, start):
+        if start:
+            os.system("open "+os.getcwd()+'/bin/startServices.sh')
+        else:
+            os.system("open "+os.getcwd()+'/bin/stopServices.sh')
 
     def openEnv(self):
         os.system("open "+os.getcwd()+'/python_env.txt')
