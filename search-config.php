8ab2d30844a1f0556d66d5c2149050bca68b5b69
matzk <kolsch@nps.edu>
2014-08-28 16:05:04 -0700

    straight doxygen site

---
+++ search-config.php
@@ -0,0 +1,22 @@
+<script language="php">
+
+$config = array(
+  'PROJECT_NAME' => "EasyCV",
+  'GENERATE_TREEVIEW' => false,
+  'DISABLE_INDEX' => false,
+);
+
+$translator = array(
+  'search_results_title' => "Search Results",
+  'search_results' => array(
+    0 => "Sorry, no documents matching your query.",
+    1 => "Found <b>1</b> document matching your query.",
+    2 => "Found <b>\$num</b> documents matching your query. Showing best matches first.",
+  ),
+  'search_matches' => "Matches:",
+  'search' => "Search",
+  'split_bar' => "",
+  'logo' => "Generated on Thu Aug 28 2014 14:28:18 for EasyCV by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.5 ",
+);
+
+</script>
