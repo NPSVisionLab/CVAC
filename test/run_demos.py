ceb85488324de7725a2898cdda4e856056ca8897
Thomas Batcha <trbatcha@nps.edu>
2014-12-01 17:06:21 -0800

    removed the start and stop services, letting the script do that

--- test/run_demos.py
+++ test/run_demos.py
@@ -26,11 +26,11 @@ if __name__ == '__main__' :
             os.putenv("PATH", pathenv + ":" + thisPath + "/../3rdparty/Ice/lib")
         # Need to stop any old services incase we had a crash last time
         # This gets rid of the lock file incase its still there
-        subprocess.call([binPath + "/stopServices" + extension], shell=True)
-        time.sleep(2)
-        print("Starting " + binPath + "/startServices" + extension)
-        subprocess.call([binPath + "/startServices" + extension], shell=True)
-        time.sleep(8)
+        #subprocess.call([binPath + "/stopServices" + extension], shell=True)
+        #time.sleep(2)
+        #print("Starting " + binPath + "/startServices" + extension)
+        #subprocess.call([binPath + "/startServices" + extension], shell=True)
+        #time.sleep(8)
         finalRes = 0
         pobj = subprocess.Popen([sys.executable, demoPath + '/prerequisites.py'])
         print("prereq id {0}".format(pobj.pid))
@@ -92,20 +92,19 @@ if __name__ == '__main__' :
         #if res == 1:
         #    print("bootstrapping.py failed")
         #    finalRes = 1
-        time.sleep(5)
-        try:
-            subprocess.call([binPath + "/stopServices" + extension], shell=True)
-        except:
-            pass
+        ##try:
+        #    subprocess.call([binPath + "/stopServices" + extension], shell=True)
+        #except:
+        #    pass
     except Exception as e:
         #failure so set error return
         print(e)
         finalRes = 1
         # If we get an error or forced quit we still want to stop the services
-        try:
-            subprocess.call([binPath + "/stopServices" + extension], shell=True)
-        except:
-            pass
+        #try:
+        #    subprocess.call([binPath + "/stopServices" + extension], shell=True)
+        #except:
+        #    pass
 
     exit(finalRes)
 
